c1f59474e1e5292261f2fa0577cb5b9a926ac7f3
Trevor <TrevorBrown25@gmail.com>
2017-09-01 16:38:52 -0700

    added the rest of the front end functionality and saved all the things to the server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+var searches = []
+
+
+app.post('/api/search', function(req, res){
+    searches.push(req.body);
+    res.status(200).json(searches);
+})
+
+app.get('/api/search', function(req, res){
+    
+        res.status(200).json(searches);
+    } )
+
+app.listen(3000, function(){
+    console.log("working on 3000");
+})
